fc7c6a1a2622ad6a357abbdae77cab294a5d6bc5
David Strba <david.strba@maternia.cz>
2021-09-02 15:40:06 +0200

    cooking recipe for valencia meal

--- src/Application/Controllers/CookingController.php
+++ src/Application/Controllers/CookingController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Application\Controllers;
 
-use App\Application\Services\AdviceService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -12,10 +11,116 @@ class CookingController extends BaseController
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        
+        $query = $request->getQueryParams();
+        $quantity = (int)($query['quantity'] ?? 800);
+        $avg = (float)($query['avg'] ?? 2.5);
+
+        $recipes = [
+            'valencia' => [
+                'couscous' => 1,
+                'datePalmWine' => 2,
+                'figPie' => 2,
+                'hamburg' => 1,
+                'teffSandwich' => 1,
+            ],
+            'couscous' => [
+                'freekehSnake' => 1,
+                'nutmeg' => 3,
+                'teffDough' => 6,
+                'paprika' => 4, 
+            ],
+            'freekehSnake' => [
+                'freekeh' => 6,
+                'water' => 5,
+                'snake' => 3,
+                'anise' => 2,
+            ],
+            'datePalmWine' => [
+                'datePalm' => 5,
+                'liquor' => 2,
+                'leavening' => 4,
+                'sugar' => 1,
+            ],
+            'figPie' => [
+                'dough' => 3,
+                'fig' => 5,
+                'oliveOil' => 2,
+                'sugar' => 3,
+            ],
+            'hamburg' => [
+                'lion' => 4,
+                'nutmeg' => 3,
+                'pickled' => 2,
+                'teffBread' => 4,
+            ],
+            'teffSandwich' => [
+                'freekehSnake' => 1,
+                'grilledScorpion' => 1,
+                'redSauce' => 3,
+                'teffBread' => 1,
+            ],
+        ];
+
+        $weights = [
+            'valencia' => 0.1,
+            'couscous' => 0.1,
+            'datePalmWine' => 0.1,
+            'figPie' => 0.1,
+            'hamburg' => 0.1,
+            'teffSandwich' => 0.1,
+            'freekehSnake' => 0.1,
+            'nutmeg' => 0.1,
+            'teffDough' => 0.1,
+            'paprika' => 0.1,
+            'datePalm' => 0.1,
+            'liquor' => 0.01,
+            'leavening' => 0.01,
+            'sugar' => 0.01,
+            'dough' => 0.1,
+            'fig' => 0.1,
+            'oliveOil' => 0.01,
+            'lion' => 0.03,
+            'pickled' => 0.1,
+            'teffBread' => 0.1,
+            'grilledScorpion' => 0.1,
+            'redSauce' => 0.01,
+        ];
+
+        $names = [
+            'valencia' => 'Valencia meal',
+            'couscous' => 'Couscous',
+            'datePalmWine' => 'Date palm wine',
+            'figPie' => 'Fig pie',
+            'hamburg' => 'King of jungle hamburg',
+            'teffSandwich' => 'Teff sandwich',
+            'freekehSnake' => 'Freekeh snake stew',
+            'nutmeg' => 'Nutmeg',
+            'teffDough' => 'Teff dough',
+            'paprika' => 'Paprika',
+            'datePalm' => 'Date palm',
+            'liquor' => 'Essence of liquor',
+            'leavening' => 'Leavening agent',
+            'sugar' => 'Sugar',
+            'dough' => 'Dough',
+            'fig' => 'Fig',
+            'oliveOil' => 'Olive oil',
+            'lion' => 'Lion meat',
+            'pickled' => 'Pickled vegetable',
+            'teffBread' => 'Teff bread',
+            'grilledScorpion' => 'Grilled scorpion',
+            'redSauce' => 'Red sauce',
+        ];
+
+        $res = [
+            'quantity' => $quantity,
+            'avg' => $avg,
+        ];
 
         return $this->render($response, TPL_DIR . '/cooking.tpl.php', [
-          
+            'res' => $res,
+            'names' => $names,
+            'recipes' => $recipes,
+            'weights' => $weights,
         ]);
     }
 }
--- src/templates/cooking.tpl.php
+++ src/templates/cooking.tpl.php
@@ -1,21 +1,45 @@
+<?php
+
+/** @var array $res */
+$totalQuantity = $res['quantity'];
+$avgCook = $res['avg'];
+?>
+
 <div class="row mb-4">
-    <div class="col-9"><h1 class="h2">Valencia meal</h1></div>
+    <div class="col-9">
+        <h1 class="h2"><?= $res['quantity'] * $res['avg'] ?>x Valencia meal</h1>
+    
+        <ul class="list-group col-6">
+            <?php $totalWeight = 0 ?>
+            <?php foreach ($recipes['valencia'] as $ingredient => $quantity): ?>
+                <li class="list-group-item">
+                    <span class="badge bg-info rounded-pill"><?= $res['quantity'] * $quantity ?></span>
+                    <?= $names[$ingredient] ?>
+                </li>
+                <?php $totalWeight += $weights[$ingredient] * $res['quantity'] * $quantity ?>
+            <?php endforeach; ?>
+            <li class="list-group-item">
+                Total weight:
+                <strong><?= $totalWeight ?></strong>
+            </li>
+        </ul>
+    </div>
     <div class="col-3">
         <div class="card">
             <div class="card-body">
                 <form method="get">
                     <div class="input-group mb-3">
                         <span class="input-group-text">Craft quantity</span>
-                        <input type="text" class="form-control" name="quantity" value="800">
+                        <input type="text" class="form-control" name="quantity" value="<?= $totalQuantity ?>">
                     </div>
                     <div class="input-group mb-3">
                         <span class="input-group-text">Avg rate</span>
-                        <input type="text" class="form-control" name="avg" value="2.5">
+                        <input type="text" class="form-control" name="avg" value="<?= $avgCook ?>">
                     </div>
-                    <div class="input-group mb-3">
+                    <!-- <div class="input-group mb-3">
                         <span class="input-group-text">Spec rate</span>
                         <input type="text" class="form-control" name="avg" value="0.3">
-                    </div>
+                    </div> -->
                     <div class="d-grid">
                         <button class="btn btn-primary">
                             Save
@@ -27,26 +51,45 @@
     </div>
 </div>
 
-
-<div class="row">
+<div class="row mb-4">
     <div class="col-4">
         <div class="card">
-            <div class="card-header">stage #1</div>
-            <div class="card-body"></div>
+            <div class="card-header">preparation</div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">An item</li>
+                </ul>
+            </div>
         </div>
     </div>
 
-    <div class="col-4">
-        <div class="card">
-            <div class="card-header">stage #2</div>
-            <div class="card-body"></div>
+    <?php foreach ($recipes['valencia'] as $meal => $mealQuantity): ?>
+        <div class="col-4 mb-4">
+            <div class="card">
+                <div class="card-header">
+                    <?= $totalQuantity * $mealQuantity ?>x 
+                    <?= $names[$meal] ?>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <?php $qMultiplier = $totalQuantity * $mealQuantity / $avgCook ?>
+                        <?php $totalWeight = 0 ?>
+                        <?php foreach ($recipes[$meal] as $ingredient => $quantity): ?>
+                            <?php $ingredientQuantity = $qMultiplier * $quantity ?>
+                            <?php $totalWeight += $weights[$ingredient] * $ingredientQuantity ?>
+                            <li class="list-group-item">
+                                <span class="badge bg-info rounded-pill"><?= $ingredientQuantity ?></span>
+                                <?= $names[$ingredient] ?>
+                            </li>
+                        <?php endforeach; ?>
+                        <li class="list-group-item">
+                            Total weight:
+                            <strong><?= $totalWeight ?></strong>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-
-    <div class="col-4">
-        <div class="card">
-            <div class="card-header">stage #3</div>
-            <div class="card-body"></div>
-        </div>
-    </div>
+    <?php endforeach; ?>
 </div>
